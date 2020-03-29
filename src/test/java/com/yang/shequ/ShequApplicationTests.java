4f8f756da7a9d03ee921013775feb225a773e992
yang.wenqiang <yang.wenqiang@moretv.com.cn>
2020-03-29 17:09:50 +0800

    init

---
+++ src/test/java/com/yang/shequ/ShequApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yang.shequ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShequApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
