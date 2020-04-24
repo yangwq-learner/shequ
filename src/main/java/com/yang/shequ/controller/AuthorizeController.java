75d7e5216576056c49d41926b3591f32dd7ff9a2
code_ywq <447087502@qq.com>
2020-04-24 10:36:53 +0800

    extract github config from code  to properties

---
+++ src/main/java/com/yang/shequ/controller/AuthorizeController.java
@@ -0,0 +1,45 @@
+package com.yang.shequ.controller;
+
+import com.yang.shequ.dto.AccessTokenDTO;
+import com.yang.shequ.dto.GithubUser;
+import com.yang.shequ.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * created by ywq on 2020/04/24
+ */
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.client.secrect}")
+    private String clientSecrect;
+
+    @Value("${github.refirect.uri}")
+    private String refirectUri;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name="code")  String code,
+                           @RequestParam(name="state") String state){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecrect);
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri(refirectUri);
+        accessTokenDTO.setState(state);
+        String accessToken =  githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        System.out.println(githubUser.getName());
+        return "index";
+    }
+}
---
+++ src/main/java/com/yang/shequ/provider/GithubProvider.java
@@ -0,0 +1,53 @@
+package com.yang.shequ.provider;
+
+import com.alibaba.fastjson.JSON;
+import com.yang.shequ.dto.AccessTokenDTO;
+import com.yang.shequ.dto.GithubUser;
+import okhttp3.*;
+import org.springframework.stereotype.Component;
+
+
+import java.io.IOException;
+
+/**
+ * Created by ywq on 2020/04/24
+ */
+@Component
+public class GithubProvider {
+    public String getAccessToken(AccessTokenDTO accessTokenDTO) {
+        MediaType mediaType = MediaType.get("application/json; charset=utf-8");
+        OkHttpClient client = new OkHttpClient();
+        RequestBody body = RequestBody.create(JSON.toJSONString(accessTokenDTO),mediaType);
+        Request request = new Request.Builder()
+                .url("https://github.com/login/oauth/access_token")
+                .post(body)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            String string =  response.body().string();
+            String token = string.split("&")[0].split("=")[1];
+            System.out.println(string);
+            return token;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public GithubUser getUser(String accessToken){
+        OkHttpClient client = new OkHttpClient();
+        String url = "https://api.github.com/user?access_token=" + accessToken;
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            String string =  response.body().string();
+            GithubUser githubUser = JSON.parseObject(string, GithubUser.class);
+            return githubUser;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return  null;
+    }
+}
